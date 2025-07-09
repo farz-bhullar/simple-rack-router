00aa7b6f56fbf15209999bd14e3bd29ef43bc149
Farz Bhullar <farz.bhullar@gmail.com>
2025-07-10 00:08:53 +0530

    Rack-based router with static and dynamic route support

---
+++ application.rb
@@ -0,0 +1,25 @@
+require_relative './router'
+class Application
+  def self.router
+    router = Router.new
+
+    router.get("/") do |req|
+      "<h1>Welcome to Simple Rack Router</h1>"
+    end
+
+    router.get("/about") do |req|
+      "<h1>About Page</h1>"
+    end
+
+    router.get("/users/:id") do |req|
+      id = req.params[:id]
+      "<h1>User Page</h1><p>User ID: #{id}</p>"
+    end
+
+    router.post("/submit") do |req|
+      "You submitted: #{req.params.inspect}"
+    end
+
+    router
+  end
+end
